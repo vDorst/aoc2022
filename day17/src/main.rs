77d382a80e07fdba35e3872357a6fc72a199c4ba
René van Dorst <opensource@vdorst.com>
2022-12-19 21:44:32 +0100

    day17: part2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,7 +1,9 @@
-use std::{io::Read, time::Instant};
+use std::{io::Read, time::Instant, default};
 
 const FIELD_WIDTH: usize = 7;
-const FIELD_HEIGHT: usize = 4096;
+const FIELD_HEIGHT: usize = 10 * 1024;
+
+const FIELD_PART2: u64 = 1_000_000_000_000;
 
 mod pos {
     pub const EMPTY: u8 = b'.';
@@ -22,6 +24,12 @@ impl Timer {
     }
 }
 
+#[derive(Debug)]
+struct State {
+    height: u32,
+    blocks: u32,
+}
+
 fn main() {
     let mut total = Timer::new();
     let mut timer = Timer::new();
@@ -29,23 +37,75 @@ fn main() {
     let mut f = std::fs::File::open("input/input.txt").unwrap();
     let mut input = Vec::<u8>::with_capacity(1_000_000);
 
+    const INPUT: &[u8] = b">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
+
     f.read_to_end(&mut input).unwrap();
 
     timer.update("Load data");
 
     let mut sim = PlayField::new(input.as_slice());
+    //let mut sim = PlayField::new(INPUT);
 
     sim.sim(2022);
     println!("Units: {}", sim.max());
 
     timer.update("Part1");
 
-    //println!("num: {cnt}");
+    sim.sim(6000);
+    println!("Units: {}", sim.max());
+
+    let (line, step) = sim.detect_pattern().expect("Found something");
+    timer.update("Part2: Detect pattern.");
+
+    let (start_line, start_block, blocks) = 'block_search: { 
+             for i in 0..sim.state.len() {
+            let start_b = &sim.state[i]; 
+            if (start_b.height as usize) < line { continue }
+            for j in i+1..sim.state.len()  {
+                let search = &sim.state[j];
+                if Shapes::from(start_b.blocks) == Shapes::from(search.blocks) && start_b.height + u32::try_from(step).unwrap() == search.height {
+                    println!("{:?} vs {:?}: {}", start_b, search, search.blocks - start_b.blocks );
+                    break 'block_search Some((start_b.height, start_b.blocks, search.blocks - start_b.blocks));
+                }
+            }
+        }
+        None
+    }.expect("No block found");
+
+
+    println!("\nStart [ line {start_line} Block {start_block} ] | Step: {step}, blocks {blocks}");
+
+    let n = (FIELD_PART2 - start_block as u64) / (blocks as u64);
+
+    let repeat = n * blocks as u64;
+
+    let remain = FIELD_PART2 - start_block as u64 - repeat;
+
+    println!("resume @ {repeat} remain: {remain}");
+
+
+    let heigth_end = sim
+        .state
+        .iter()
+        .find(|s| s.blocks >= start_block + remain  as u32  - 1)
+        .map(|s| s.height)
+        .unwrap();
+
+    let total_height = n * step as u64 + heigth_end as u64;
+
+    println!(
+        "{FIELD_PART2} = {total_height} - {}",
+        1514285714288_u64.abs_diff(total_height)
+    );
+
+    timer.update("Part2");
 
     total.update("Total");
 }
 
+#[derive(Debug, PartialEq, Eq, Default)]
 enum Shapes {
+    #[default]
     Min,
     Plus,
     RightAngle,
@@ -74,6 +134,16 @@ impl Shapes {
             Shapes::Block => Shape(vec![(0, 2), (0, 2)]),
         }
     }
+
+    fn as_number(&self) -> u8 {
+        match self {
+            Shapes::Min => 0,
+            Shapes::Plus => 1,
+            Shapes::RightAngle => 2,
+            Shapes::Pipe => 3,
+            Shapes::Block => 4,
+        }
+    }
 }
 
 struct Shape(Vec<(u8, u8)>);
@@ -84,26 +154,19 @@ struct PlayField<'a> {
     field: [[u8; FIELD_HEIGHT]; FIELD_WIDTH],
     blocks: u32,
     height: usize,
+    /// Heigth, Block, Shape
+    state: Vec<State>,
 }
 
 impl<'a> PlayField<'a> {
     fn new(input: &'a [u8]) -> Self {
-        //let x = [EMPTY; 4098];
         Self {
             input,
             step: 0,
-            // field: [
-            //     x.clone(),
-            //     x.clone(),
-            //     x.clone(),
-            //     x.clone(),
-            //     x.clone(),
-            //     x.clone(),
-            //     x.clone(),
-            // ],
             field: [[pos::EMPTY; FIELD_HEIGHT]; FIELD_WIDTH],
             blocks: 0,
             height: 0,
+            state: Vec::with_capacity(FIELD_HEIGHT),
         }
     }
 
@@ -136,7 +199,7 @@ impl<'a> PlayField<'a> {
         let min = self.min();
         let max = self.max();
 
-        for line in (min ..=max + 10).rev() {
+        for line in (min..=max + 10).rev() {
             print!("|");
             for h in &self.field {
                 let c = if let Some(c) = h.get(line - 1) {
@@ -163,8 +226,7 @@ impl<'a> PlayField<'a> {
             // Ended?
             let mut new_block = row == 0;
             //if max >= row
-            if !new_block
-            {
+            if !new_block {
                 for (egde, top) in shape.0.iter().zip(&self.field[x..]) {
                     // println!("--- Step: {} Top {}, Row {row} edge ({},{}) ", self.step, top, egde.0, egde.1);
                     //if row + usize::from(egde.0) <= top.len() {
@@ -179,12 +241,15 @@ impl<'a> PlayField<'a> {
 
             if new_block {
                 for (egde, top) in shape.0.iter().zip(&mut self.field[x..]) {
-                    for n in egde.0..egde.0+egde.1 {
-                        top[row + usize::from(n) ] = b'#';
+                    for n in egde.0..egde.0 + egde.1 {
+                        top[row + usize::from(n)] = b'#';
                         self.height = self.height.max(row + usize::from(n) + 1);
                     }
                     // println!("Step: {} Top {}, Row {row} edge ({},{}) ", self.step, *top, egde.0, egde.1);
                 }
+
+                self.state.push( State { height: self.height as u32, blocks: self.blocks});
+
                 self.blocks += 1;
                 // Done?
                 if self.blocks == blocks {
@@ -219,20 +284,11 @@ impl<'a> PlayField<'a> {
                 e => panic!("Unknown Move {e}"),
             };
 
-            print!("\tCan move X {x} -> {new_x:?}: ");
+            // print!("\tCan move X {x} -> {new_x:?}: ");
 
             if let Some(new_x) = new_x {
                 let mut can_move = true;
 
-                // if shape
-                //     .0
-                //     .iter()
-                //     .zip(&self.field[new_x..])
-                //     .any(|(bottom_edge, stack_top)| {
-                //         row + usize::from(bottom_edge.0) >= stack_top.len()
-                //     })
-                // {}
-                // Can maybe move
                 for (edge, top) in shape.0.iter().zip(&self.field[new_x..]) {
                     for n in edge.0..edge.0 + edge.1 {
                         if let Some(b'#') = top.get(row + usize::from(n) - 1) {
@@ -241,16 +297,15 @@ impl<'a> PlayField<'a> {
                         }
                     }
                 }
-                
 
                 if can_move {
-                    println!("Moved");
+                    //println!("Moved");
                     x = new_x
                 } else {
-                    println!("Block");
+                    //println!("Block");
                 }
             } else {
-                println!("Edge!");
+                //println!("Edge!");
             }
 
             // Next row
@@ -260,6 +315,51 @@ impl<'a> PlayField<'a> {
                 self.step = 0;
             }
         }
+    }
+
+    fn detect_pattern(&self) -> Option<(usize, usize)> {
+        let mut list = Vec::<u16>::with_capacity(self.height);
+
+        let mut state = self.state.iter(); 
+
+        let mut shape = state.next().unwrap();
+
+        for i in 0..self.height {
+            let mut top_row = 0;
+
+            for b in 0..FIELD_WIDTH {
+                if self.field[b][i] == pos::BLOCK {
+                    top_row |= 1 << b;
+                }
+            }
+            if usize::try_from(shape.height).unwrap() == i {
+                shape = state.next().unwrap();
+            }
+
+            top_row |= u16::try_from(shape.blocks % 5).unwrap() << 8;
+            list.push(top_row);
+        }
+
+        for step in 6..list.len() {
+            for line in 1..list.len() - step {
+                let mut found = 0;
+
+                'line: for start in (line..list.len()).step_by(step).skip(1) {
+                    for (num, search) in (start..(start + step).min(self.height)).enumerate() {
+                        if list[line + num] != list[search] {
+                            break 'line;
+                        }
+                    }
+                    found += 1;
+
+                    if found == ((self.height - line) / step) - 1 {
+                        println!("### Line: {line} step {step} Found {found}");
+                        return Some((line, step));
+                    }
+                }
+            }
+        }
+        None
     }
 }
 
@@ -319,6 +419,8 @@ mod tests {
         sim.draw();
 
         assert_eq!(sim.max(), 3068);
+
+        assert_eq!(sim.detect_pattern(), Some((25, 53)));
     }
 
     #[test]
@@ -340,5 +442,9 @@ mod tests {
         assert_eq!(sim.max(), 2);
 
         assert_eq!(sim.len_map(), [0, 0, 1, 1, 1, 2, 2]);
+
+        for state in sim.state.iter() {
+            println!("{:?}", state);
+        }
     }
 }
